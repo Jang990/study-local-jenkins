48c87dc2111b187de5b402528096eb3766be2856
Jang_Desktop <sdsd090811@gmail.com>
2023-05-14 16:33:50 +0900

    fix slf4j

--- src/main/java/Hello/jenkins/test/TestController.java
+++ src/main/java/Hello/jenkins/test/TestController.java
@@ -1,11 +1,9 @@
 package Hello.jenkins.test;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Slf4j
 public class TestController {
     @GetMapping("")
     public String test() {

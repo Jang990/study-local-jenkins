7dcf9f2cd297bc2cac8e0a3c2fda23c579822c12
Jang_Desktop <sdsd090811@gmail.com>
2023-05-14 15:52:07 +0900

    ssh test2

--- src/main/java/Hello/jenkins/test/TestClass.java
+++ src/main/java/Hello/jenkins/test/TestClass.java
@@ -3,5 +3,5 @@ package Hello.jenkins.test;
 public class TestClass {
     private Long id; // Test
     private String webhookTest; // ? ss
-    private String test; // testCommit // test2
+    private String test; // testCommit // test23s
 }

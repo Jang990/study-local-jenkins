20ad5b538cc3ae77fcdc941e65a8fe3af0e3156a
Jang_Desktop <sdsd090811@gmail.com>
2023-10-20 19:38:31 +0900

    test commit 12

--- src/main/java/Hello/jenkins/test/TestClass.java
+++ src/main/java/Hello/jenkins/test/TestClass.java
@@ -2,6 +2,6 @@ package Hello.jenkins.test;
 
 public class TestClass {
     private Long id; // Test
-    private String webhookTest; // ? ssss
+    private String webhookTest; // ? sssss
     private String test; // testCommit // test23ssdasdasd
 }
